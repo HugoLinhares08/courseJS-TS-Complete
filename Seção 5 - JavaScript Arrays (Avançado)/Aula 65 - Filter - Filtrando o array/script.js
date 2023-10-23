20a4a4025468cd872648e62ee79e3be3e3cb9fe2
Hugo Linhares <hugolinhares92@gmail.com>
2023-10-23 11:05:23 -0300

    Aula 65 -> Filter - Filtrando o array

--- Seção 5 - JavaScript Arrays (Avançado)/Aula 65 - Filter - Filtrando o array/script.js
+++ Seção 5 - JavaScript Arrays (Avançado)/Aula 65 - Filter - Filtrando o array/script.js
@@ -1,17 +1,17 @@
-// FIlter, map, reduce
+// FIlter -> Sempre retorna um array com a mesma quantidade de elementos ou menos.
 
 // Retorne os números maiores que 10
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
-const callbackFilter = (valor, indice, array) => {
-  return valor > 10;
-};
-
-const numerosFiltrados = numeros.filter(callbackFilter);
-console.log(numerosFiltrados);
+// const numerosFiltrados = numeros.filter((valor) => valor > 10);
+// const numerosFiltrados = numeros.filter((valor, indice, array) => {
+//   console.log(valor, indice, array);
+//   return valor > 10;
+// });
+// console.log(numerosFiltrados);
 
 // Retorne as pessoas que tem o nome com 5 letras ou mais
-// Retorne as pessoas com mais de 5 anos
+// Retorne as pessoas com mais de 50 anos
 // Retorne as pessoas cujo nome termina com a
 const pessoas = [
   { nome: "Luiz", idade: 62 },
@@ -21,6 +21,23 @@ const pessoas = [
   { nome: "Rosana", idade: 32 },
   { nome: "Wallace", idade: 47 },
 ];
+
+// const pessoasNomeGrande = pessoas.filter((valor) => valor.nome.length >= 5);
+// pessoasNomeGrande.forEach((pessoas) => {
+//   console.log(`Nome: ${pessoas.nome}, Idade: ${pessoas.idade}`);
+// });
+
+// const pessoasMaisVelhas = pessoas.filter((valor) => valor.idade > 50);
+// pessoasMaisVelhas.forEach((pessoas) => {
+//   console.log(`Nome: ${pessoas.nome}, Idade: ${pessoas.idade}`);
+// });
+
+const nomesTerminadosComA = pessoas.filter((valor) =>
+  valor.nome.toLowerCase().endsWith("a")
+);
+nomesTerminadosComA.forEach((pessoas) => {
+  console.log(`Nome: ${pessoas.nome}, Idade: ${pessoas.idade}`);
+});
 
 // // Callback Function Example
 // function greet(name, myFunction) {
